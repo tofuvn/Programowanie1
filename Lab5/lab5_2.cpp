b7aada75c82239fdf553b00e2bd5f5b7c4375d4c
The Anh <thehovnpl@gmail.com>
2021-11-22 21:41:23 +0100

    add lab 5

--- Lab5/lab5_2.cpp
+++ Lab5/lab5_2.cpp
@@ -1,43 +1,46 @@
 #include <iostream>
 #include <stdlib.h>  
 #include <cmath>
-#include <iomanip>
 
 
 using namespace std;
 
 int main() {
 	int *first, *second;
-	int first_N = 3, second_N = 2;
+	int sizeOfFirstArray = 3, sizeOfSecondArray = 2;
 	
-	std::setw(50);
+	/////// a. Wielomiany
 	
-	first = (int*)malloc(first_N * sizeof(int));
-	second = (int*)malloc(second_N * sizeof(int));
+	// Allocate memory for first and second array
+	first = (int*)malloc(sizeOfFirstArray * sizeof(int));
+	second = (int*)malloc(sizeOfSecondArray * sizeof(int));
 	
-	for (int i = 0; i < first_N; i++) {
+	
+	// Initialize random coefficients for each polynomial
+	for (int i = 0; i < sizeOfFirstArray; i++) {
 		*(first + i) = rand() % 10;
 	}
 	
-	for (int i = 0; i < second_N; i++) {
+	for (int i = 0; i < sizeOfSecondArray; i++) {
 		*(second + i) = rand() % 10;
 	}
 	
-	int max_coff_sum = max(first_N, second_N);
+	// Degree of sum 2 polynomial is equal max value degree of 2 polynomial
+	int max_coff_sum = max(sizeOfFirstArray, sizeOfSecondArray);
 	int *sum = (int*)malloc(max_coff_sum * sizeof(int));
 	
-	
-	int diff = abs(first_N - second_N);
+	// Different degree of 2 polynomials
+	int diff = abs(sizeOfFirstArray - sizeOfSecondArray);
 	
 	for (int i = 0; i < max_coff_sum; i++) {
 		if (i < diff) {
-			if (first_N < second_N) {
+			if (sizeOfFirstArray < sizeOfSecondArray) {
 				*(sum + i) = *(second + i);
 			} else {
 				*(sum + i) = *(first + i);
 			}	
 		} else {
-			if (first_N < second_N) {
+			if (sizeOfFirstArray < sizeOfSecondArray) {
 				*(sum + i) = *(second + i) + *(first + i - diff);
 			} else {
 				*(sum + i) = *(second + i - diff) + *(first + i);
@@ -46,8 +49,9 @@ int main() {
 		}
 	}
 	
-	
-	int max_coff_multi = first_N + second_N - 1;
+	// Degree of multi 2 polynomial is equal value of first degree + second degree - 1
+
+	int max_coff_multi = sizeOfFirstArray + sizeOfSecondArray - 1;
 	
 	int *mul = (int*)malloc(max_coff_multi * sizeof(int));
 	
@@ -55,37 +59,75 @@ int main() {
 		*(mul + i) = 0;
 	}
 	
-	for (int i = 0; i < first_N; i++) {
-		for(int j = 0; j < second_N; j++) {
+	for (int i = 0; i < sizeOfFirstArray; i++) {
+		for(int j = 0; j < sizeOfSecondArray; j++) {
 			mul[i+j] += (*(first + i) * *(second+j)); 
 			}
 	}
 	
-	
-	
-	
-	
-	
-
-	// Print
-	for (int i = 0; i < first_N; i++) {
+	// Display
+	cout << "First: ";
+	for (int i = 0; i < sizeOfFirstArray; i++) {
 		
 		cout << *(first + i) << " ";
 	}
 	cout << '\n';
 	
-	for (int i = 0; i < second_N; i++) {
+	
+	cout << "Second: ";
+	for (int i = 0; i < sizeOfSecondArray; i++) {
 		cout<< *(second + i) << " ";
 	}
 	cout << '\n';
 	
+	cout << "Sum: " ;
 	for (int i = 0; i < max_coff_sum; i++) {
 		cout<< *(sum + i) << " ";
 	}
 	cout << '\n';
+	
+	cout << "Multi : ";
 	for (int i = 0; i < max_coff_multi; i++) {
 		cout << *(mul + i) << " ";
 	}
+	cout << '\n';
 	
+	/////// b.Filtr 
+	int sizeOfArray = 10;
+	
+	// Allocate memory and initialize values
+	int* arrayOfRandomValues = (int*) malloc (sizeof(int) * sizeOfArray);
+	for (int i = 0; i < sizeOfArray; i++) {
+		*(arrayOfRandomValues + i) = rand() % 10;
+	}
+	
+	cout << "Array : ";
+	for (int i = 0; i < sizeOfArray; i++) {
+		cout << *(arrayOfRandomValues + i) << " ";
+	}
+	cout << '\n';
+	
+	
+	/////// c.Systemy Pozycyjne
+	
+	int number = 8;
+	int size = 5;
+	int *arrayInTernary = new int[size];
+	
+	int divide = number;
+	int count = 0;
+	do {
+		int remainder = divide % 3;
+		divide = divide / 3;
+		arrayInTernary[count] = remainder;
+		count++;
+		
+	}while (divide > 0);
+	
+	cout << "Decimal: " << number << endl;
+	cout << "Ternany: ";
+	for (int i = count - 1; i >= 0; i-- ) {
+		cout << arrayInTernary[i] << " ";
+	}
 	return 0;
 }
--- Lab5/lab5_3.cpp
+++ Lab5/lab5_3.cpp
@@ -14,22 +14,41 @@ int main() {
 	
 	int const N = 10;
 	
-	long long* factorial = (long long*) malloc ((N + 1) * sizeof(long long));
+	// Could be 
+	// long long * factorial = new long long[N + 1]
+ 	long long* factorial = (long long*) malloc ((N + 1) * sizeof(long long));
 	
+	// Calculate factorial for N first numbers
 	allFactorial(factorial, N);
+	
+	
 	
 	int** newton = (int**)malloc(N * sizeof(int*));
 	
 	for (int i = 0; i < N; i++) {
-		newton[i] = (int*)malloc((N + 1)  * sizeof(int));
+		newton[i] = (int*)malloc((i + 1)  * sizeof(int));
 	}
 	
-	for (int i = 0; i <=N; i++) {
-		cout << *(factorial + i) << " ";
+	// C(a, b) = a! / ( b! * (a - b)! )
+	
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < i + 1; j++) {
+			newton[i][j] = factorial[i] / (factorial[j] * factorial[i-j]);
+		}
 	}
 	
+	for (int i = 0; i < 10; i++) {
+		cout << factorial[i] << " ";
+	}
 	
-	
+	cout << '\n';
+	for (int i = 0; i < N; i++) {
+		cout << "Degree : " << i << " = " ; 
+		for (int j = 0; j < i + 1; j++) {
+			cout << newton[i][j] << " ";
+		}
+		cout << '\n';
+	}
 	return 0;
 }
 
