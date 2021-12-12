c6ad33901cbde1f8fbfa81eff05a609461872c56
The Anh <thehovnpl@gmail.com>
2021-12-12 02:00:01 +0100

    add Homework3

---
+++ Homework/ZadanieDomowe3/cmake-build-debug/CMakeFiles/ZadanieDomowe3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZadanieDomowe3.dir/main.cpp.o"
+  "ZadanieDomowe3"
+  "ZadanieDomowe3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZadanieDomowe3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
