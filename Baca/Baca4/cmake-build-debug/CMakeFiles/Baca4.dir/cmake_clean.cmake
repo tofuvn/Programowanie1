65a2dce8883cd168b5fd8ffca54e1b108c3ed62c
The Anh <thehovnpl@gmail.com>
2021-12-22 16:44:14 +0100

    Add BACA 4

---
+++ Baca/Baca4/cmake-build-debug/CMakeFiles/Baca4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Baca4"
+  "Baca4.pdb"
+  "CMakeFiles/Baca4.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Baca4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
